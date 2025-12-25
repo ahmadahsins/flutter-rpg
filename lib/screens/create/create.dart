245028f8eb3f503c033a55c2f33c4cf6e4a20436
Ahmad Ahsin Syamil <118179124+ahmadahsins@users.noreply.github.com>
2025-12-25 20:43:41 +0700

    feat: screens navigation

--- lib/screens/create/create.dart
+++ lib/screens/create/create.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_rpg/models/character.dart';
 import 'package:flutter_rpg/models/vocation.dart';
 import 'package:flutter_rpg/screens/create/vocation_card.dart';
+import 'package:flutter_rpg/screens/home/home.dart';
 import 'package:flutter_rpg/shared/styled_button.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
@@ -57,6 +58,8 @@ class _CreateState extends State<Create> {
         id: uuid.v1(),
       ),
     );
+
+    Navigator.push(context, MaterialPageRoute(builder: (ctx) => const Home()));
   }
 
   @override
