188a2cc84e968a596ca0e6d56e7a783db64e6412
Ahmad Ahsin Syamil <118179124+ahmadahsins@users.noreply.github.com>
2025-12-25 21:07:17 +0700

    feat: add dialog

--- lib/screens/create/create.dart
+++ lib/screens/create/create.dart
@@ -42,11 +42,43 @@ class _CreateState extends State<Create> {
   // submit handler
   void handleSubmit() {
     if (_nameController.text.trim().isEmpty) {
-      // show error message
+      showDialog(
+        context: context,
+        builder: (ctx) {
+          return AlertDialog(
+            title: const StyledHeading('Missing Character Name'),
+            content: const StyledText(
+              'Every good RPG character needs a great name...',
+            ),
+            actions: [
+              StyledButton(
+                onPressed: () => Navigator.pop(ctx),
+                child: const StyledHeading('Close'),
+              ),
+            ],
+            actionsAlignment: MainAxisAlignment.center,
+          );
+        },
+      );
       return;
     }
     if (_sloganController.text.trim().isEmpty) {
-      // show error message
+      showDialog(
+        context: context,
+        builder: (ctx) {
+          return AlertDialog(
+            title: const StyledHeading('Missing Character Slogan'),
+            content: const StyledText('Remember to add a catchy saying...'),
+            actions: [
+              StyledButton(
+                onPressed: () => Navigator.pop(ctx),
+                child: const StyledHeading('Close'),
+              ),
+            ],
+            actionsAlignment: MainAxisAlignment.center,
+          );
+        },
+      );
       return;
     }
 
