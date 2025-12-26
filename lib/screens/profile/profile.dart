a15b6f108fbaad2fc48dc0bc0e15c17302b75cd7
Ahmad Ahsin Syamil <118179124+ahmadahsins@users.noreply.github.com>
2025-12-26 21:40:38 +0700

    feat: add hero and animated rotation animations

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -27,10 +27,13 @@ class Profile extends StatelessWidget {
               color: AppColors.secondaryColor.withValues(alpha: 0.3),
               child: Row(
                 children: [
-                  Image.asset(
-                    'assets/img/vocations/${character.vocation.image}',
-                    width: 140,
-                    height: 140,
+                  Hero(
+                    tag: character.id.toString(),
+                    child: Image.asset(
+                      'assets/img/vocations/${character.vocation.image}',
+                      width: 140,
+                      height: 140,
+                    ),
                   ),
                   const SizedBox(width: 20),
                   Expanded(
--- lib/screens/profile/stats_table.dart
+++ lib/screens/profile/stats_table.dart
@@ -13,6 +13,8 @@ class StatsTable extends StatefulWidget {
 }
 
 class _StatsTableState extends State<StatsTable> {
+  double turns = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -25,11 +27,15 @@ class _StatsTableState extends State<StatsTable> {
             padding: const EdgeInsets.all(8),
             child: Row(
               children: [
-                Icon(
-                  Icons.star,
-                  color: widget.character.points > 0
-                      ? Colors.yellow
-                      : Colors.grey,
+                AnimatedRotation(
+                  turns: turns,
+                  duration: const Duration(milliseconds: 200),
+                  child: Icon(
+                    Icons.star,
+                    color: widget.character.points > 0
+                        ? Colors.yellow
+                        : Colors.grey,
+                  ),
                 ),
                 const SizedBox(width: 20),
                 const StyledText('Stat points available:'),
@@ -71,6 +77,8 @@ class _StatsTableState extends State<StatsTable> {
                         setState(() {
                           widget.character.increaseStats(stat['title']!);
                         });
+
+                        turns += 0.5;
                       },
                       icon: Icon(
                         Icons.arrow_upward,
@@ -86,6 +94,8 @@ class _StatsTableState extends State<StatsTable> {
                         setState(() {
                           widget.character.decreaseStats(stat['title']!);
                         });
+
+                        turns -= 0.5;
                       },
                       icon: Icon(
                         Icons.arrow_downward,
