448cee1fc87b26c2816a6d237137dcab37670597
Ahmad Ahsin Syamil <118179124+ahmadahsins@users.noreply.github.com>
2025-12-26 13:04:47 +0700

    feat: add global state management

--- lib/screens/create/create.dart
+++ lib/screens/create/create.dart
@@ -3,22 +3,24 @@ import 'package:flutter_rpg/models/character.dart';
 import 'package:flutter_rpg/models/vocation.dart';
 import 'package:flutter_rpg/screens/create/vocation_card.dart';
 import 'package:flutter_rpg/screens/home/home.dart';
+import 'package:flutter_rpg/screens/services/character_store.dart';
 import 'package:flutter_rpg/shared/styled_button.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 import 'package:uuid/uuid.dart';
 
 var uuid = Uuid();
 
-class Create extends StatefulWidget {
-  const Create({super.key});
+class CreateScreen extends StatefulWidget {
+  const CreateScreen({super.key});
 
   @override
-  State<Create> createState() => _CreateState();
+  State<CreateScreen> createState() => _CreateScreenState();
 }
 
-class _CreateState extends State<Create> {
+class _CreateScreenState extends State<CreateScreen> {
   final _nameController = TextEditingController();
   final _sloganController = TextEditingController();
 
@@ -52,7 +54,9 @@ class _CreateState extends State<Create> {
             ),
             actions: [
               StyledButton(
-                onPressed: () => Navigator.pop(ctx),
+                onPressed: () {
+                  Navigator.pop(ctx);
+                },
                 child: const StyledHeading('Close'),
               ),
             ],
@@ -60,6 +64,7 @@ class _CreateState extends State<Create> {
           );
         },
       );
+
       return;
     }
     if (_sloganController.text.trim().isEmpty) {
@@ -79,15 +84,16 @@ class _CreateState extends State<Create> {
           );
         },
       );
+
       return;
     }
 
-    characters.add(
+    Provider.of<CharacterStore>(context, listen: false).addCharacter(
       Character(
         name: _nameController.text.trim(),
         slogan: _sloganController.text.trim(),
         vocation: selectedVocation,
-        id: uuid.v1(),
+        id: uuid.v4(),
       ),
     );
 
